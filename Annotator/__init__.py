da52c5b64817fd484dea6bce436d380ebf45f70c
ntjess <ntjess@rocketmail.com>
2020-01-21 09:02:52 -0500

    minor refactoring
    Former-commit-id: 897ec61f42ed8089a7e7056c2f3547fa56747c72

--- Annotator/__init__.py
+++ Annotator/__init__.py
@@ -1,8 +1,13 @@
 import pyqtgraph as pg
+from pyqtgraph.Qt import QtWidgets
+import sys
 # Makes sure that when the folder is run as a module, the app exists in the outermost
 # scope of the application
-appInst = pg.mkQApp()
-
+appInst = QtWidgets.QApplication.instance()
+if appInst is None:
+  appInst = QtWidgets.QApplication(sys.argv)
+# Now that the app was created with sys args, populate pg instance
+pg.mkQApp()
 # Change names so they can be labeled on export
 from .Annotator import Annotator
 from .tablemodel import makeCompDf
