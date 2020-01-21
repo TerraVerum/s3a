e44f283d18f7028b03154145de5c72e727847ce5
ntjess <ntjess@rocketmail.com>
2020-01-21 09:02:58 -0500

    code for performance testing
    Former-commit-id: c8b80b283e0343e12a37b2fd28f1f77cf7444944

---
+++ Tests/test_performance.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numpy.random import randint
+from pandas import DataFrame as df
+import pyqtgraph as pg
+import string
+from time import time
+from tqdm import tqdm
+
+from Annotator.tablemodel import makeCompDf
+from Annotator.constants import  TEMPLATE_COMP as TC
+from Annotator import Annotator, makeCompDf, appInst
+
+np.random.seed(42)
+outTimes = {}
+
+maxCompSz = 200
+
+lettersArr = np.array(list(string.ascii_letters + string.digits))
+fnames = [f'C:/Users/ntjes/Desktop/Git/ImgAnnotator/Images/{name}.tif' for name in ['fast', 'med', 'orig']]
+for inImg in tqdm(fnames, 'Files'):
+  outTimes[inImg] = {}
+  win = Annotator(inImg)
+  winImgShape = win.mainImg.image.shape
+  for numComps in tqdm([round(10**x) for x in [2,3]], 'Num Comps'):
+    comps = makeCompDf(numComps)
+    xVerts = randint(winImgShape[1], size=(randint(maxCompSz),1, numComps))
+    yVerts = randint(winImgShape[0], size=xVerts.shape)
+    allVerts = np.hstack((xVerts, yVerts))
+    allVerts_list = [allVerts[:,:,ii] for ii in range(numComps)]
+    notes = [''.join(lettersArr[randint(52, size=randint(30))]) for ii in range(numComps)]
+    devTxt = [''.join(lettersArr[randint(52, size=randint(30))]) for ii in range(numComps)]
+    boardTxt = [''.join(lettersArr[randint(52, size=randint(30))]) for ii in range(numComps)]
+    comps[TC.VERTICES.name] = allVerts_list
+    comps[TC.NOTES.name] = notes
+    comps[TC.DEV_TEXT.name] = devTxt
+    comps[TC.BOARD_TEXT.name] = boardTxt
+    timeToAdd = time()
+    win.compMgr.addComps(comps)
+    timeToAdd = time() - timeToAdd
+    win.compMgr.rmComps()
+    outTimes[inImg][numComps] = timeToAdd
+outDf = df(outTimes)
+outDf.to_csv('./outTimes.csv')
+pg.plot(outTimes)
