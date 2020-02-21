2cf0755bbf3a5fdcab9ff2872d6066538c2bb8a1
Nathan Jessurun <ntjess@rocketmail.com>
2020-02-21 01:14:07 -0500

    hooks up draw widget to focused image
    Former-commit-id: 1dfb31a7da68d71c4c9eb90dbc794d4751b8d60a

--- Annotator/FRGraphics/drawopts.py
+++ Annotator/FRGraphics/drawopts.py
@@ -8,16 +8,22 @@ from ..params import FRParam
 
 class FRDrawOpts(QtWidgets.QWidget):
   def __init__(self, parent=None, shapes: Tuple[FRParamGroup, ...]=None, actions: Tuple[FRParamGroup, ...]=None):
+    """
+    Creates a draw options widget hosting both shape and action selection buttons.
+    :param parent: UI widget whose destruction will also destroy these widgets
+    :param shapes: Shape options that will appear in the widget
+    :param actions: Action optiosn that will appear in the widget
+    """
     super().__init__(parent)
     # Create 2 layout versions so on resize the group boxes can 'wrap'
     self.topLayout = QtWidgets.QHBoxLayout()
     self.setLayout(self.topLayout)
 
     # SHAPES
-    shapeUiGroup, self.shapeBtnGroup, self.shapeBtns = self._create_addBtnToGroup(shapes)
+    shapeUiGroup, self.shapeBtnGroup, self.shapeBtnParamMap = self._create_addBtnToGroup(shapes)
     self.topLayout.addWidget(shapeUiGroup)
     # ACTIONS
-    actionUiGroup, self.actionBtnGroup, self.actionBtns = self._create_addBtnToGroup(actions)
+    actionUiGroup, self.actionBtnGroup, self.actionBtnParamMap = self._create_addBtnToGroup(actions)
     self.topLayout.addWidget(actionUiGroup)
     self.topLayout.setDirection(self.topLayout.LeftToRight)
     self.horizWidth = self.layout().minimumSize().width()
@@ -30,18 +36,19 @@ class FRDrawOpts(QtWidgets.QWidget):
     super().resizeEvent(ev)
 
     # RESIZE BEHAVIOR
-  def _create_addBtnToGroup(self, whichBtns: FR_CONSTS) -> Tuple:
+  def _create_addBtnToGroup(self, whichBtns: FR_CONSTS) \
+      -> Tuple[QtWidgets.QGroupBox, QtWidgets.QButtonGroup, Dict[QtWidgets.QPushButton, FRParam]]:
     uiGroup = QtWidgets.QGroupBox(self)
     uiLayout = QtWidgets.QHBoxLayout(uiGroup)
     btnGroup = QtWidgets.QButtonGroup(uiGroup)
-    btnDict: Dict[FRParam, QtWidgets.QPushButton] = {}
+    btnDict: Dict[QtWidgets.QPushButton, FRParam] = {}
     for btnParam in whichBtns: # type: FRParam
       # TODO: Use picture instead of name
       newBtn = QtWidgets.QPushButton(btnParam.name, uiGroup)
       newBtn.setCheckable(True)
       btnGroup.addButton(newBtn)
       uiLayout.addWidget(newBtn)
-      btnDict[btnParam] = newBtn
+      btnDict[newBtn] = btnParam
     return uiGroup, btnGroup, btnDict
 
 def _create_addActionsToToolbar(toolbar: QtWidgets.QToolBar, whichBtns: FR_CONSTS) -> QtWidgets.QActionGroup:
