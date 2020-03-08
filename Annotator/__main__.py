a50c1e04146fc7800221c987be0f7c2f825070f9
ntjess <ntjess@rocketmail.com>
2020-03-08 15:43:18 -0400

    loads large image by default
    Former-commit-id: b2f0552b678e6fa1fd301ef2cf2d6fe44d909398

--- Annotator/__main__.py
+++ Annotator/__main__.py
@@ -7,7 +7,7 @@ from . import appInst
 from .Annotator import Annotator
 from Annotator.projectvars.constants import BASE_DIR
 
-startImgFpath = path.join(BASE_DIR, '../Images/fast.png')
+startImgFpath = path.join(BASE_DIR, '../Images/orig.png')
 if len(sys.argv) > 1:
   startImgFpath = path.join(BASE_DIR, sys.argv[1])
 win = Annotator(startImgFpath)
