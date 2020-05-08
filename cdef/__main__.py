e61b1f9527c70f73569e0730c112fe99917eaf7b
Nathan Jessurun <ntjess@rocketmail.com>
2020-05-08 13:43:05 -0400

    refactors interface to cdef instantiation

--- cdef/__main__.py
+++ cdef/__main__.py
@@ -8,21 +8,21 @@ from . import appInst
 from .cdefapp import FRCdefApp
 
 
-def main(author: str = None, gui=True, **profileArgs) -> Optional[FRCdefApp]:
+def main(gui=True, **profileArgs) -> Optional[FRCdefApp]:
   """
   Calling code for the CDEF application.
 
-  :param author: Required if no default author exists for the application.
-    The default author is updated every time an author name is given.
   :param gui: Whether to run in the Qt event loop or not. If false, the user can inject
     interactions into the app using the returned :class:`FRCdefApp` object. Otherwise (default),
     the GUI application is shown and the Qt event loop is executed.
+  :key author: Required if no default author exists for the application.
+    The default author is updated every time an author name is given.
   :key Image: Optional initial image to be annotated
   :key Annotations: Optional initial annotation file loaded.
   """
   # Parameter editors are named in title-case, so ensure this is how keys are formatted
   profileArgs = {k.title(): v for k, v in profileArgs.items()}
-  win = FRCdefApp(author, profileArgs)
+  win = FRCdefApp(**profileArgs)
   if gui:
     QtCore.QTimer.singleShot(0, win.showMaximized)
     sys.exit(appInst.exec())
--- cdef/cdefapp.py
+++ cdef/cdefapp.py
@@ -50,7 +50,7 @@ class FRCdefApp(FRAnnotatorUI):
         FR_CONSTS.PROP_EST_BOUNDS_ON_START)
     cls.useDarkTheme = FR_SINGLETON.scheme.registerProp(cls, FR_CONSTS.SCHEME_USE_DARK_THEME)
 
-  def __init__(self, authorName: str = None, userProfileArgs: Dict[str, Any]=None):
+  def __init__(self, **userProfileArgs: Dict[str, Any]):
     super().__init__()
     # ---------------
     # DATA ATTRIBUTES
@@ -61,7 +61,7 @@ class FRCdefApp(FRAnnotatorUI):
 
     self.statBar = QtWidgets.QStatusBar(self)
     self.setStatusBar(self.statBar)
-    authorName = resolveAuthorName(authorName)
+    authorName = resolveAuthorName(userProfileArgs.get('Author', None))
     if authorName is None:
       sys.exit('No author name provided and no default author exists. Exiting.\n'
                'To start without error, provide an author name explicitly, e.g.\n'
