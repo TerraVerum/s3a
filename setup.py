43ce58dd9f84e71d77c2316649d38b9e09611f0d
Nathan Jessurun <ntjess@rocketmail.com>
2020-05-14 22:26:31 -0400

    Allows module to install its own requirements without intervention from the user

--- setup.py
+++ setup.py
@@ -10,12 +10,21 @@ def _mltGlob(curDir, *globs):
   outFiles = [str(f) for f in outFiles]
   return outFiles
 
+here = Path(__file__).parent
+lines = (here/'requirements.txt').open('r').readlines()
+required = []
+for line in lines:
+  if not line.startswith('#'):
+    required.append(line.strip('\n'))
+
+
 setup(
   name='cdef',
   version='0.1.0',
   package_dir={'cdef':'cdef'},
   packages=find_packages(),
   data_files=[('cdef/cdef/icons', _mltGlob('cdef/icons', '*.svg', '*.png'))],
+  install_requires=required,
   include_package_data=True,
   url='https://gitlab.com/ficsresearch/cdef',
   license='',
