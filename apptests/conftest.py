03488739496b375c6c47a84f0a5096cbb79f1d11
Jessurun <njessurun@ufl.edu>
2020-07-06 11:24:59 -0400

    apparently tracking warning traces fails on docker

--- apptests/conftest.py
+++ apptests/conftest.py
@@ -18,19 +18,19 @@ dfTester = CompDfTester(NUM_COMPS)
 dfTester.fillRandomVerts(imShape=SAMPLE_IMG.shape)
 dfTester.fillRandomClasses()
 
-@pytest.fixture(scope='session', autouse=True)
-def cfg_warnings():
-  oldWarnings = warnings.showwarning
-  def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
-
-    log = file if hasattr(file,'write') else sys.stderr
-    traceback.print_stack(file=log)
-    log.write(warnings.formatwarning(message, category, filename, lineno, line))
-
-  warnings.showwarning = warn_with_traceback
-  yield
-  # Set back to normal
-  warnings.showwarning = oldWarnings
+# @pytest.fixture(scope='session', autouse=True)
+# def cfg_warnings():
+#   oldWarnings = warnings.showwarning
+#   def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
+#
+#     log = file if hasattr(file,'write') else sys.stderr
+#     traceback.print_stack(file=log)
+#     log.write(warnings.formatwarning(message, category, filename, lineno, line))
+#
+#   warnings.showwarning = warn_with_traceback
+#   yield
+#   # Set back to normal
+#   warnings.showwarning = oldWarnings
 
 @pytest.fixture(scope="module")
 def sampleComps():
