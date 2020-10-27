3d6c486b87689579e73514587f682b252fb14876
Nathan Jessurun <ntjess@rocketmail.com>
2020-10-26 23:55:55 -0400

    Significant project updates Can add image folders, cleaned thumbnail viewer, no longer allow array of annotations per image since this isn't how annotations are internally represented in the project

--- s3a/parameditors/project.py
+++ s3a/parameditors/project.py
@@ -1,5 +1,4 @@
 import shutil
-import tempfile
 from pathlib import Path
 from typing import List, Optional, Union, Set, Dict
 
@@ -9,7 +8,7 @@ import pandas as pd
 
 from s3a.constants import BASE_DIR, REQD_TBL_FIELDS
 from s3a.generalutils import resolveYamlDict
-from s3a.graphicsutils import saveToFile
+from s3a.graphicsutils import saveToFile, popupFilePicker, PopupLineEditor
 from s3a.parameditors.table import TableData
 from s3a.structures import FilePath, NChanImg, S3AIOError
 from s3a.io import ComponentIO
@@ -23,23 +22,37 @@ def hierarchicalUpdate(curDict: dict, other: dict):
     else:
       curDict[k] = v
 
+class ThumbnailViewer(QtWidgets.QListWidget):
+  sigImageSelected = QtCore.Signal(object) # Image full path to delete
+  def __init__(self, parent=None):
+    super().__init__(parent)
+    self.nameToFullPathMapping: Dict[str, Path] = {}
+    self.setViewMode(self.IconMode)
+    self.setIconSize(QtCore.QSize(200,200))
+    self.setResizeMode(self.Adjust)
+
+  def addThumbnail(self, fullName: Path):
+    icon = QtGui.QIcon(str(fullName))
+    newItem = QtWidgets.QListWidgetItem(fullName.name)
+    newItem.setIcon(icon)
+    self.addItem(newItem)
+    self.nameToFullPathMapping[fullName.name] = fullName
+
 class ProjectData:
   def __init__(self):
     self.tableData = TableData()
     self.cfg = {}
     self.cfgFname: Optional[Path] = Path()
-    self.images: Set[Path] = set()
-    self.imgToAnnMapping: Dict[Path, List[Path]] = {}
+    self.images: List[Path] = []
+    self.baseImgDirs: Set[Path] = set()
+    self.imgToAnnMapping: Dict[Path, Path] = {}
     """Records annotations belonging to each image"""
     self.exportOpts = {}
 
     self.compIo = ComponentIO()
     self.compIo.tableData = self.tableData
 
-    self._thumbnails = QtWidgets.QListWidget()
-    self._thumbnails.setViewMode(self._thumbnails.IconMode)
-    self._thumbnails.setIconSize(QtCore.QSize(200,200))
-    self._thumbnails.setResizeMode(self._thumbnails.Adjust)
+    self._thumbnails = ThumbnailViewer()
 
   @property
   def location(self):
@@ -50,7 +63,6 @@ class ProjectData:
   @property
   def annotationsDir(self):
       return self.location/'annotations'
-
 
   def loadCfg(self, cfgFname: FilePath=None, cfgDict: dict = None):
     _, defaultCfg = resolveYamlDict(BASE_DIR/'projectcfg.yml')
@@ -91,6 +103,10 @@ class ProjectData:
     shouldCopy = self.cfg['import-opts']['copy-data']
     for image in self.cfg['images']:
       if not isinstance(image, dict):
+        image = Path(image)
+        if image.is_dir():
+          self.addImageFolder(image, shouldCopy)
+          continue
         image = {'name': image}
       self.addImage(**image, copyToProj=shouldCopy)
     for annotation in self.cfg['annotations']:
@@ -109,48 +125,69 @@ class ProjectData:
     strImgNames = []
     strAnnNames = []
     location = self.location
+    for folder in self.baseImgDirs:
+      if location in folder.parents:
+        folder = folder.relative_to(location)
+      strImgNames.append(str(folder))
     for img in self.images:
-      if location in img.parents:
-        outName = str(img.relative_to(location))
+      if img.parent in self.baseImgDirs:
+          # This image is already accounted for in the base directories
+          continue
+      strImgNames.append(str(img))
+    for ann in self.imgToAnnMapping.values():
+      if location in ann.parents:
+        outName = str(ann.relative_to(location))
       else:
-        outName = str(img)
-      strImgNames.append(outName)
-    for annList in self.imgToAnnMapping.values():
-      for ann in annList:
-        if location in ann.parents:
-          outName = str(ann.relative_to(location))
-        else:
-          outName = str(ann)
-        strAnnNames.append(outName)
+        outName = str(ann)
+      strAnnNames.append(outName)
     self.cfg['images'] = strImgNames
     self.cfg['annotations'] = strAnnNames
     saveToFile(self.cfg, self.cfgFname)
 
   def addImage(self, name: FilePath, data: NChanImg=None, copyToProj=False):
-    name = Path(name)
-    fullName = name.absolute()
+    name = Path(name).resolve()
     if copyToProj or data is not None:
       name = self._copyImgToProj(name, data)
-    self.images.add(name)
-    icon = QtGui.QIcon(str(fullName))
-    newItem = QtWidgets.QListWidgetItem(fullName.name)
-    newItem.setIcon(icon)
-    self._thumbnails.addItem(newItem)
+    if name not in self.images:
+      self.images.append(name)
+    self._thumbnails.addThumbnail(name)
+
+  def addImageFolder(self, folder: FilePath, copyToProj=False):
+    folder = Path(folder)
+    if copyToProj:
+      newFolder = self.imagesDir/folder.name
+      shutil.copytree(folder, newFolder)
+      folder = newFolder
+      copyToProj = False
+    self.baseImgDirs.add(folder)
+    for img in folder.glob('*.*'):
+      self.addImage(img, copyToProj=copyToProj)
+
+  def addImage_gui(self, copyToProject=True):
+    fileFilter = "Image Files (*.png *.tif *.jpg *.jpeg *.bmp *.jfif);;All files(*.*)"
+    fname = popupFilePicker(self, 'Add Image to Project', fileFilter)
+    if fname is not None:
+      self.addImage(fname, copyToProj=copyToProject)
 
   def removeImage(self, imgName: FilePath):
-    imgName = Path(imgName).absolute()
-    self.images.discard(imgName)
+    imgName = Path(imgName).resolve()
+    self.images.remove(imgName)
     # Remove copied annotations for this image
     for ann in self.annotationsDir.glob(f'{imgName.stem}.*'):
       ann.unlink()
     self.imgToAnnMapping.pop(imgName, None)
 
+  def removeImage_gui(self):
+    self._thumbnails.show()
+    self._thumbnails.exec_()
+
+
   def removeAnnotation(self, annName: FilePath):
-    annName = Path(annName).absolute()
+    annName = Path(annName).resolve()
     # Since no mapping exists of all annotations, loop the long way until the file is found
-    for annList in self.imgToAnnMapping.values():
-      if annName in annList:
-        annList.remove(annName)
+    for key, ann in self.imgToAnnMapping.items():
+      if annName == ann:
+        del self.imgToAnnMapping[key]
         break
 
   def addAnnotation(self, name: FilePath=None, data: pd.DataFrame=None, image: FilePath=None):
@@ -164,19 +201,18 @@ class ProjectData:
     if image is None:
       # If no explicit matching to an image is provided, try to determine based on annotation name
       image = self._getImgForAnn(Path(name))
-    image = Path(image).absolute()
+    image = Path(image).resolve()
     # Since only one annotation file can exist per image, concatenate this with any existing files for the same image
     # if needed
-    annsForImg = self.imgToAnnMapping.get(image, {})
-    # For now this is a method of batch-loading instead of forcing individual file read and appends
-    oldAnns = [ComponentIO.buildByFileType(ann) for ann in annsForImg]
+    annForImg = self.imgToAnnMapping.get(image, None)
+    oldAnns = ComponentIO.buildByFileType(annForImg)
     combinedAnns = oldAnns + [data]
     outAnn = pd.concat(combinedAnns, ignore_index=True)
     outAnn[REQD_TBL_FIELDS.INST_ID] = outAnn.index
     outFmt = f".{self.exportOpts['annotation-format']}"
     outName = self.annotationsDir / image.with_suffix(outFmt).name
     ComponentIO.exportByFileType(outAnn, outName, verifyIntegrity=False, readOnly=False, imgDir=self.imagesDir)
-    annsForImg[image] = outName
+    self.imgToAnnMapping[image] = outName
 
   def _copyImgToProj(self, name: Path, data: NChanImg=None):
     newName = self.imagesDir/name.name
@@ -189,7 +225,7 @@ class ProjectData:
     else:
       raise S3AIOError(f'No image data associated with {name.name}. Either the file does not exist or no'
                        f' image information was provided.')
-    return newName.absolute()
+    return newName.resolve()
 
   def _getImgForAnn(self, name: Path):
     matchName = name.stem
