3b330f42d8c2947eb5998eb2366e456d4dde8db3
Nathan Jessurun <ntjess@rocketmail.com>
2021-04-28 15:52:54 -0400

    Adds --version printout from command line, deprecates help since it didn't work anyway

--- s3a/__main__.py
+++ s3a/__main__.py
@@ -9,14 +9,14 @@ from .views.s3agui import S3A
 from utilitys.fns import makeExceptionsShowDialogs
 
 
-def main(loadLastState=True, help=False, **profileArgs):
+def main(loadLastState=True, version=False, **profileArgs):
   """
   Calling code for the S3A application.
 
   :param loadLastState: When the app is closed, all settings are saved. If this is *True*,
     these settings are restored on startup. If *False*, they aren't.
 
-  :param help: Show help documentation
+  :param version: Show version information
 
   :key image: Optional initial image to be annotated
   :key annotations: Optional initial annotation file loaded.
@@ -24,9 +24,11 @@ def main(loadLastState=True, help=False, **profileArgs):
     This can be e.g. `colorscheme`, `shortcuts`, etc.
   """
   # Handle here for faster bootup
-  win = S3A(guiMode=not help, loadLastState=loadLastState, **profileArgs)
-  if help:
-    return win
+  if version:
+    from .__version__ import __version__
+    print(__version__)
+    return
+  win = S3A(guiMode=True, loadLastState=loadLastState, **profileArgs)
   makeExceptionsShowDialogs(win)
   QtCore.QTimer.singleShot(0, win.showMaximized)
   sys.exit(appInst.exec_())
