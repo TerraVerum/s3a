9346747858607313d70055f0c424ffec4e91c104
Nathan Jessurun <ntjessu@gmail.com>
2021-07-15 16:30:21 -0400

    Defers shared imports to avoid circular reference error

--- s3a/shared.py
+++ s3a/shared.py
@@ -1,20 +1,14 @@
-import logging
-
 from pyqtgraph.Qt import QtCore
-
-from s3a.constants import GEN_PROPS_DIR, SCHEMES_DIR, SHORTCUTS_DIR, IMG_PROC_DIR, \
-  MULT_PRED_DIR, CFG_DIR
-from s3a.parameditors.algcollection import AlgCollection
-from s3a.parameditors.quickloader import QuickLoaderEditor
-from s3a.parameditors.table import TableData
-from s3a.plugins.misc import SettingsPlugin, ShortcutsPlugin
-from s3a.processing import ImgProcWrapper, ImageProcess
-from utilitys import ActionStack, ParamEditor, ShortcutParameter, dockPluginFactory
-from utilitys.fns import AppLogger
-import logging
 
 class SharedAppSettings(QtCore.QObject):
   def __init__(self, parent=None):
+    from s3a.constants import IMG_PROC_DIR, MULT_PRED_DIR, CFG_DIR
+    from s3a.parameditors.algcollection import AlgCollection
+    from s3a.parameditors.table import TableData
+    from s3a.plugins.misc import SettingsPlugin, ShortcutsPlugin
+    from s3a.processing import ImgProcWrapper, ImageProcess
+    from utilitys import ActionStack
+
     super().__init__(parent)
     self.actionStack = ActionStack()
 
