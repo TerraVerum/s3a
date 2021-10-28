6e954197ad9d1d031aa184847b5ff619a41d4e3a
njessurun <ntjessu@gmail.com>
2021-10-27 21:04:20 -0400

    No longer requires explicit opencv/python versions 🎉 - Highly experimental, subject to change. But allows defaulting to alternative library providers if functionality already exists

--- s3a/shims.py
+++ s3a/shims.py
@@ -6,8 +6,8 @@ import sys
 __all__ = ['entry_points', 'typing_extensions']
 
 if sys.version_info < (3, 8):
-    from importlib_metadata import entry_points
-    import typing_extensions
+  from importlib_metadata import entry_points
+  import typing_extensions
 else:
-    from importlib.metadata import entry_points
-    import typing as typing_extensions
+  from importlib.metadata import entry_points
+  import typing as typing_extensions
--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pkg_resources
 from setuptools import setup, find_packages
 
 __version__ = ''
@@ -15,48 +16,76 @@ def _mltGlob(curDir, *globs):
   return outFiles
 
 here = Path(__file__).parent
-lines = (here/'requirements.txt').open('r').readlines()
-required = []
-for line in lines:
-  if not line.startswith('#'):
-    required.append(line.strip('\n'))
+def getRequirements():
+  lines = (here / 'requirements.txt').open('r').readlines()
+  required = []
+
+  for line in lines:
+    if not line.startswith('#'):
+      required.append(line.strip('\n'))
+  return required
+
+def getAltRequirements():
+  altRequired = []
+  for req in here.joinpath('alt-requirements.txt').open('r').readlines():
+    parts = req.split('# test ')
+    module = parts[0].strip()
+    if len(parts) < 2:
+      # No test available
+      altRequired.append(module)
+    obj = parts[1].strip()
+    # noinspection PyBroadException
+    try:
+      # Test should raise assertion error on failure
+      scope = dict(VERSION=pkg_resources.parse_version)
+      exec(obj, scope, scope)
+    except (AssertionError, ModuleNotFoundError):
+      # Module not available or test went badly, assume the worst
+      altRequired.append(module)
+  return altRequired
+
 
 # Get the long description from the README file
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
-setup(
-  name='s3a',
-  version=__version__,
-  package_dir={'s3a':'s3a'},
-  packages=find_packages(),
-  data_files=[
-    ('s3a/s3a/icons', _mltGlob('s3a/icons', '*.svg', '*.png')),
-    ('s3a/s3a', _mltGlob('s3a/cfg', '*.yml')),
-    ('s3a/s3a/compio/templates', _mltGlob('s3a/compio/templates', '*.tblcfg')),
-  ],
-  entry_points={
-    'console_scripts': [
-      's3a-gui = s3a.__main__:main_cli',
-    ]
-  },
+INSTALL_REQUIRES = getRequirements() + getAltRequirements()
 
-  install_requires=required,
-  include_package_data=True,
-  url='https://gitlab.com/ficsresearch/s3a',
-  download_url='https://gitlab.com/ficsresearch/s3a',
-  license='MIT',
-  description='Semantic labeling made simple',
-  long_description=long_description,
-  long_description_content_type='text/markdown',
-  keywords='semantic, labeling, image, annotation',
-  author='Nathan Jessurun',
-  author_email='njessurun@ufl.edu',
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
-  ],
-  python_requires='>=3.7.*',
-)
+
+if __name__ == '__main__':
+  setup(
+    name='s3a',
+    version=__version__,
+    package_dir={'s3a':'s3a'},
+    packages=find_packages(),
+    data_files=[
+      ('s3a/s3a/icons', _mltGlob('s3a/icons', '*.svg', '*.png')),
+      ('s3a/s3a', _mltGlob('s3a/cfg', '*.yml')),
+      ('s3a/s3a/compio/templates', _mltGlob('s3a/compio/templates', '*.tblcfg')),
+    ],
+    entry_points={
+      'console_scripts': [
+        's3a-gui = s3a.__main__:main_cli',
+      ]
+    },
+
+    install_requires=INSTALL_REQUIRES,
+    include_package_data=True,
+    url='https://gitlab.com/ficsresearch/s3a',
+    download_url='https://gitlab.com/ficsresearch/s3a',
+    license='MIT',
+    description='Semantic labeling made simple',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords='semantic, labeling, image, annotation',
+    author='Nathan Jessurun',
+    author_email='njessurun@ufl.edu',
+    classifiers=[
+      'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+      'Intended Audience :: Developers',      # Define that your audience are developers
+      'Topic :: Software Development :: Build Tools',
+      'License :: OSI Approved :: MIT License',   # Again, pick a license
+      'Programming Language :: Python :: 3',      #Specify which python versions that you want to support
+    ],
+    python_requires='>=3.7.*',
+  )
+
