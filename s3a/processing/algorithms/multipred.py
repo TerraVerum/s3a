TerraVerum/s3a

commit a5d674771dbf04a6a64372583f85903143998fd7

multipred.py updates - Add function to call `ComplexXYVertices.simplify` on multipred result - Rename `simplify_components` -> `mereg_overlapping_components` since new function exists to simplify component vertices - Preserve metadata in updated `merge_overlapping_components` function

s3a/processing/algorithms/multipred.py
@@ -237,19 +237,47 @@ def make_grid_components(
     df[RTF.VERTICES] = boxes
     return ProcessIO(components=df)
 
-
-def simplify_components(components: pd.DataFrame):
+def merge_overlapping_components(components: pd.DataFrame):
     """
-    Simplifies a list of components by merging adjacent/overlapping regions
-    :param components: Dataframe of components to simplify
+    Creates new list of components where adjacent regions are merged and everything
+    is split apart after. This means every component will have at most one region
+    (i.e. components that previously contained two separate regions will turn into
+    two distinct components with the same metadata)
+    :param components: Dataframe of components to merge -> split
     """
+    out = ProcessIO(components=components)
     if not len(components):
-        outComps = components
-    else:
-        merged = components[RTF.VERTICES].s3averts.merge()
-        newVerts = pd.Series([merged], index=[components.index[0]]).s3averts.split()
-        outComps = components.loc[newVerts.index].copy()
-        outComps[RTF.VERTICES] = newVerts
+        return out
+    mask, mapping = defaultIo.exportLblPng(components, returnLabelMapping=True)
+    numLbls, labels, stats, centroids = cv.connectedComponentsWithStats(mask.astype("uint8", copy=False))
+    # Use ID from each centroid as the component whose metadata is kept
+    keepIds = []
+    outVerts = []
+    for lbl in range(1, numLbls):
+        # Greatly speed up conversion by only checking the image region which contains
+        # this component
+        offset = stats[lbl, [cv.CC_STAT_TOP, cv.CC_STAT_LEFT]]
+        endPixs = offset + stats[lbl, [cv.CC_STAT_HEIGHT, cv.CC_STAT_WIDTH]]
+        maskSubset = labels[offset[0]:endPixs[0], offset[1]:endPixs[1]]
+        boolMaskSubset = maskSubset == lbl
+        # Keep information from first id composing the shape
+        onPixs = boolMaskSubset.nonzero()
+        keepIds.append(mask[onPixs[0][0] + offset[0], onPixs[1][0] + offset[1]])
+        # Offset is in row-col, removeOffset expects x-y
+        outVerts.append(ComplexXYVertices.fromBinaryMask(boolMaskSubset).removeOffset(-offset[::-1]))
+    outComps = components[np.isin(components[RTF.INST_ID], mapping[keepIds])].copy()
+    outComps[RTF.VERTICES] = outVerts
+    return ProcessIO(components=outComps)
+
+def simplify_component_vertices(components: pd.DataFrame, epsilon=1):
+    """
+    Runs ``ComplexXYVertices.simplify`` on each component vertices
+    :param components: Dataframe of components to simplify
+    :param epsilon: Passed to ``ComplexXYVertices.simplify``
+        limits: [-1, None]
+    """
+    outComps = components.copy()
+    outComps[RTF.VERTICES] = [v.simplify(epsilon=epsilon) for v in outComps[RTF.VERTICES]]
     return ProcessIO(components=outComps)
 
 

commit df5c3c31a62e7c4d2edb348ff92b24f41bb367e2

Bugfix for regression introduced by a5d67477. Merging overlapping comps while keeping metadata didn't account for nonsequential ids (i.e. in new components). Fix this by adding a temporary indexable column with the necessary guarantees

s3a/processing/algorithms/multipred.py
@@ -249,7 +249,14 @@ def merge_overlapping_components(components: pd.DataFrame):
     out = ProcessIO(components=components)
     if not len(components):
         return out
-    mask, mapping = defaultIo.exportLblPng(components, returnLabelMapping=True)
+    # Guarantee each component is labeled uniquely, something that is not true when
+    # components are new (i.e. many have id -1)
+    components = components.copy()
+    dummyLabel = PrjParam("__lbl_merge_overlap__", 0)
+    components[dummyLabel] = np.arange(len(components))
+    mask, mapping = defaultIo.exportLblPng(
+        components, returnLabelMapping=True, labelField=dummyLabel
+    )
     numLbls, labels, stats, centroids = cv.connectedComponentsWithStats(
         mask.astype("uint8", copy=False)
     )
@@ -270,8 +277,12 @@ def merge_overlapping_components(components: pd.DataFrame):
         outVerts.append(
             ComplexXYVertices.fromBinaryMask(boolMaskSubset).removeOffset(-offset[::-1])
         )
-    outComps = components[np.isin(components[RTF.INST_ID], mapping[keepIds])].copy()
+    oldName = components.index.name
+    components["__old_index__"] = components.index
+    indexable = components.set_index(dummyLabel)
+    outComps = indexable.loc[mapping[keepIds]].set_index("__old_index__")
     outComps[RTF.VERTICES] = outVerts
+    outComps.index.name = oldName
     return ProcessIO(components=outComps)
 
 