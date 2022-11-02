54a2104c4ac8ab9ed930d686e4b2e98612474fb3
Nathan Jessurun <ntjessu@gmail.com>
2022-11-02 18:46:46 -0400

    Update `MultiPredictionsPlugin` to new shareable behavior

--- s3a/plugins/multipred.py
+++ s3a/plugins/multipred.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import numpy as np
 import pandas as pd
-from utilitys import ProcessIO
+from qtextras import nameFormatter
 
 from .base import ProcessorPlugin
 from ..constants import (
@@ -11,9 +11,10 @@ from ..constants import (
     PRJ_CONSTS as CNST,
     PRJ_ENUMS,
     REQD_TBL_FIELDS as RTF,
+    MENU_OPTS_DIR,
 )
 from ..models.tablemodel import ComponentManager
-from ..parameditors.algcollection import AlgorithmCollection
+from ..parameditors.algcollection import AlgorithmCollection, AlgorithmEditor
 from ..processing.algorithms import multipred
 from ..shared import SharedAppSettings
 from ..structures import ComplexXYVertices
@@ -24,59 +25,63 @@ class MultiPredictionsPlugin(ProcessorPlugin):
 
     manager: ComponentManager
 
-    def __initEditorParams__(self, shared: SharedAppSettings):
-        super().__initEditorParams__()
+    def __initSharedSettings__(self, shared: SharedAppSettings = None, **kwargs):
+        super().__initSharedSettings__()
 
         self.multiPredictionCollection = AlgorithmCollection(
             saveDir=MULTI_PREDICTIONS_DIR, template=CONFIG_DIR / "multipred.yml"
         )
-        self.procEditor = self.multiPredictionCollection.createProcessorEditor(
-            type(self), self.name + " Processor"
+        self.processEditor = AlgorithmEditor(
+            self.multiPredictionCollection,
+            name=self.name + " Processor",
+            directory=MENU_OPTS_DIR / nameFormatter(type(self).__name__).lower(),
         )
-        self.dock.addEditors([self.procEditor])
+        _, self.processEditorMenu = self.processEditor.createWindowDock(
+            self.window, self.processEditor.name
+        )
         self.pluginRunnerProc = self.multiPredictionCollection.parseProcessName(
             "Run Plugins", topFirst=False
         )
 
     def __init__(self):
         super().__init__()
-        self.registerFunc(self.lastRunAnalytics)
+        self.registerFunction(self.lastRunAnalytics)
 
-    def attachWinRef(self, win):
-        super().attachWinRef(win)
-        self.manager = win.componentManager
-        self.mainImage = win.mainImage
-        win.mainImage.toolsEditor.registerFunc(
+    def attachToWindow(self, window):
+        super().attachToWindow(window)
+        self.manager = window.componentManager
+        self.mainImage = window.mainImage
+        window.mainImage.toolsEditor.registerFunction(
             self.makePrediction,
-            btnOpts=CNST.TOOL_MULT_PRED,
-            ignoreKeys=["components"],
+            runActionTemplate=CNST.TOOL_MULT_PRED,
+            ignores=["components"],
         )
         self.updateRunnerLimits()
-        win.sigPluginAdded.connect(self.updateRunnerLimits)
+        window.sigPluginAdded.connect(self.updateRunnerLimits)
 
     def updateRunnerLimits(self):
         self.pluginRunnerProc.input.parameters["plugins"].setLimits(
             [
                 p.name
-                for p in self.win.classPluginMap.values()
+                for p in self.window.classPluginMap.values()
                 if hasattr(p, "runOnComponent")
             ]
         )
 
     def makePrediction(self, components: pd.DataFrame = None, **runKwargs):
-        if self.win.mainImage.image is None:
+        if self.window.mainImage.image is None:
             return
         if components is None:
-            components = self.win.componentDf
+            components = self.window.componentDf
         # It is possible for a previously selected id to be deleted before a redraw
         # occurs, in which case the selected id won't correspond to a valid index.
         # Resolve using intersection with all components
         selectedIds = np.intersect1d(
-            self.win.componentController.selectedIds,
-            self.win.componentManager.compDf.index,
+            self.window.componentController.selectedIds,
+            self.window.componentManager.compDf.index,
         )
         vbRange = np.array(self.mainImage.getViewBox().viewRange()).T
-        image = self.win.mainImage.image
+        image = self.window.mainImage.image
         result = self.currentProcessor.activate(
             components=components,
             fullComponents=components,
@@ -86,8 +91,8 @@ class MultiPredictionsPlugin(ProcessorPlugin):
             selectedIds=selectedIds,
             **runKwargs,
         )
-        if not isinstance(result, ProcessIO):
-            result = ProcessIO(components=result)
+        if not isinstance(result, dict):
+            result = dict(components=result)
         compsToAdd = result["components"]
         if not len(compsToAdd):
             return
@@ -106,8 +111,8 @@ class MultiPredictionsPlugin(ProcessorPlugin):
         # But keep them recorded to handle them later on
         emptyComps = []
         for pluginName in plugins:
-            for plugin in self.win.classPluginMap.values():
-                if plugin.name == pluginName:
+            for plugin in self.window.classPluginMap.values():
+                if plugin.name == pluginName and hasattr(plugin, "runOnComponent"):
                     dispatched = multipred.ProcessDispatcher(plugin.runOnComponent)
                     emptyIdxs = (
                         components[RTF.VERTICES]
